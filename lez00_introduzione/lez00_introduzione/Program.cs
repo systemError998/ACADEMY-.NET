77ce977684e1eddb0d3c65b53e84c890c3274cce
Anna Cerasoli <annacerasoli998@gmail.com>
2024-09-09 21:33:25 +0200

    lez00 intro

---
+++ lez00_introduzione/lez00_introduzione/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lez00_introduzione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Single line comment
+/* Multi line 
+ * comment */
+#if debug
+            Console.WriteLine("Sono in modalità debug");
+#elif RELEASE
+            Console.WriteLine("Sono in modalità Release");
+#endif
+        }
+    }
+}
